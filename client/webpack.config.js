1c294c14c2fab4fe160f9f6eea156a2a4e6fb9ee
Brendan McHugh <brendanmchugh12@gmail.com>
2020-03-16 23:26:45 +0000

    move to webpack add webpack config jest config babel config

---
+++ client/webpack.config.js
@@ -0,0 +1,72 @@
+const path = require('path');
+const HtmlWebpackPlugin = require('html-webpack-plugin');
+const { CleanWebpackPlugin } = require('clean-webpack-plugin');
+const Dotenv = require('dotenv-webpack');
+
+const PATH_SOURCE = path.join(__dirname, './src');
+const PATH_DIST = path.join(__dirname, './dist');
+
+module.exports = env => {
+  const environment = env.environment;
+  const isProduction = environment === 'production';
+  const isDevelopment = environment === 'development';
+
+  return {
+    mode: environment,
+    devtool: 'cheap-module-eval-source-map',
+    devServer: {
+      contentBase: PATH_DIST,
+      host: 'localhost',
+      port: 8080,
+      historyApiFallback: true,
+      overlay: {
+        errors: true,
+        warnings: true
+      }
+    },
+    entry: [path.join(PATH_SOURCE, './index.js')],
+    output: {
+      path: PATH_DIST,
+      filename: 'js/[name].[hash].js'
+    },
+    module: {
+      rules: [
+        {
+          test: /\.js$/,
+          exclude: /node_modules/,
+          use: {
+            loader: 'babel-loader',
+            options: {
+              presets: [
+                [
+                  '@babel/preset-env',
+                  {
+                    debug: true,
+                    useBuiltIns: 'usage',
+                    corejs: 3
+                  }
+                ],
+                '@babel/preset-react'
+              ]
+            }
+          }
+        },
+        {
+          test: /\.s?css$/,
+          use: ['style-loader', 'css-loader', 'sass-loader']
+        },
+        {
+          test: /\.(png|svg|jpg|gif)$/,
+          use: ['file-loader']
+        }
+      ]
+    },
+    plugins: [
+      new HtmlWebpackPlugin({
+        template: path.join(PATH_SOURCE, './index.html')
+      }),
+      new CleanWebpackPlugin(),
+      new Dotenv()
+    ]
+  };
+};
